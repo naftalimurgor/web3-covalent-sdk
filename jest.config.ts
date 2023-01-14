2c78550b4019f25f6a4ce87175973b1883f24c7a
naftalimurgor <murgornaftali@gmail.com>
2023-01-14 20:53:22 +0300

    test: add `jest` test suite setup

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  automock: false,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageProvider: 'babel',
+  // https://github.com/jest-community/jest-extended: Extended matchers
+  setupFilesAfterEnv: ['jest-extended/all', './setEnvVar'],
+  roots: ['src/__tests__'],
+  coveragePathIgnorePatterns: [
+    'node_modules/',
+    'src/__tests__/utils/*',
+    'src/constants.ts',
+    'src/types.ts',
+    'src/abis/',
+  ],
+  testPathIgnorePatterns: [
+    'node_modules/',
+    'src/__tests__/utils/*',
+    'src/constants.ts',
+    'src/types.ts',
+    'src/abis/',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(level-sublevel/node_modules/lgtgt)/)',
+  ],
+  transform: {
+    '\\.[jt]s?$': 'babel-jest',
+  },
+};
+
+export default config;
