33741949ece8967a1bfc9db798bb17d58967e76d
naftalimurgor <murgornaftali@gmail.com>
2023-01-14 11:18:57 +0300

    test: [wip]: add unit tests

--- src/lib/v1/v1.spec.ts
+++ src/lib/v1/v1.spec.ts
@@ -3,3 +3,10 @@
 // Transactions
 // Balances
 // Base
+import test from 'ava';
+
+import { Balances } from '../v1/Balances';
+import { Base } from '../v1/Base';
+import { Transactions } from '../v1/Transactions';
+import { DEX } from '../v1/DEX';
+import { NFTs } from '../v1/NFTS';
