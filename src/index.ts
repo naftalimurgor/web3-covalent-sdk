f0b2a0de7ccf3a75265cb85cde6b2a1b14a1cfaa
naftalimurgor <murgornaftali@gmail.com>
2023-01-14 11:18:38 +0300

    update `Web3CovalentSDK`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Balances } from './lib/v1/Balances';
+import { Base } from './lib/v1/Base';
+import { DEX } from './lib/v1/DEX';
 import { NFTs } from './lib/v1/NFTS';
 import { Transactions } from './lib/v1/Transactions';
 import { SDKConfig } from './types';
@@ -7,12 +9,16 @@ class Web3CovalentSDK {
   public readonly balances: Balances;
   public readonly transactions: Transactions;
   public readonly nfts: NFTs;
+  public readonly dexes: DEX;
+  public readonly base: Base;
 
   constructor(config: SDKConfig) {
     this.balances = new Balances(config);
     this.transactions = new Transactions(config);
     this.nfts = new NFTs(config);
+    this.dexes = new DEX(config);
+    this.base = new Base(config);
   }
 }
 
-export = { Web3CovalentSDK, Transactions };
+export = { Web3CovalentSDK, Transactions, Balances, DEX, NFTs, Base };
