4d3a4fa5ee07599252bc42cc7250ef877ed312bc
naftalimurgor <murgornaftali@gmail.com>
2023-01-19 14:09:07 +0300

    update `get` function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosHeaders } from 'axios'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function get(url: string, headers?: AxiosHeaders) {
     try {
         const result = await axios.get(url, { headers })
-        return result
+        return result.data
     } catch (error) {
         return error
     }
